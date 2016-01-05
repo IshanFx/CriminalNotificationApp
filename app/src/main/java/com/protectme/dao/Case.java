810941e0a7d61b1f4a9081a8bf4a6a17ff4d2a4c
unknown <ishantuf@gmail.com>
2016-01-05 00:27:00 -0800

    Design Added

---
+++ app/src/main/java/com/protectme/dao/Case.java
@@ -0,0 +1,11 @@
+package com.protectme.dao;
+
+/**
+ * Created by IshanFx on 1/5/2016.
+ */
+public class Case {
+    private int id;
+    private String startDate;
+    private String startTime;
+
+}
---
+++ app/src/main/java/com/protectme/dao/User.java
@@ -0,0 +1,14 @@
+package com.protectme.dao;
+
+/**
+ * Created by IshanFx on 1/5/2016.
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int status;
+
+}
